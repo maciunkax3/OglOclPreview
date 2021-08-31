8346d21e4bfafc8d3e00b7819f7662eb436da1cd
Maciej Plewka <plewkamaciek@gmail.com>
2021-08-31 17:58:10 +0200

    Avg fps counter
    Signed-off-by: Maciej Plewka <plewkamaciek@gmail.com>

--- app/src/main/java/com/example/myapplication/FPSCounter.java
+++ app/src/main/java/com/example/myapplication/FPSCounter.java
@@ -5,11 +5,19 @@ import android.util.Log;
 public class FPSCounter {
     long startTime = System.nanoTime();
     int frames = 0;
+    int framesAvg = 0;
+    int cycles = 0;
 
     public void logFrame() {
         frames++;
+        framesAvg++;
         if(System.nanoTime() - startTime >= 1000000000) {
-            Log.d("FPSCounter", "fps: " + frames);
+            cycles++;
+            //Log.d("FPSCounter", "fps: " + frames);
+            if(cycles == 15) {
+                Log.d("FPSCounter AVG", "fps: " + framesAvg / cycles);
+                cycles = 0;framesAvg = 0;
+            }
             frames = 0;
             startTime = System.nanoTime();
         }
